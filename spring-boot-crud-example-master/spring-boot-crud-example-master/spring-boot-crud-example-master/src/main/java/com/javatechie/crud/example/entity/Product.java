f3f42c702a353e7e00e1397c08d92800b1851dd9
Aniket gitHub <aniketij98@gmail.com>
2025-03-24 19:15:25 +0530

    added project file

---
+++ spring-boot-crud-example-master/spring-boot-crud-example-master/spring-boot-crud-example-master/src/main/java/com/javatechie/crud/example/entity/Product.java
@@ -0,0 +1,62 @@
+package com.javatechie.crud.example.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+    private int quantity;
+    private double price;
+
+    // Constructors
+    public Product() {}
+
+    public Product(int id, String name, int quantity, double price) {
+        this.id = id;
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
